e11fa00353867caa7eb34ac10603cdd00a045caa
Sebastian Zawrzykraj <48572502+ZebaSetas@users.noreply.github.com>
2021-05-28 12:23:01 -0300

    Revert "Ãinitial commit"
    This reverts commit 1f7c28419c6fe05354e7a2274d8c74508372b32c.

--- src/app/models/smart-value-statistic.ts
+++
@@ -1,8 +0,0 @@
-export class SmartValueStatitic {
-    value: number;
-    name:string 
-    constructor(value: number, name:string){
-        this.value = value;
-        this.name = name;
-    }
-}
--- src/app/models/state.ts
+++
@@ -1,8 +0,0 @@
-export class State {
-    id: number;
-    value:string 
-    constructor(id: number, value:string){        
-        this.id = id;
-        this.value = value;
-    }
-}
