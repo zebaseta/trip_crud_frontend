3b583f1bf10755868bea440e447ec897b2e76b7f
Sebastian Zawrzykraj <48572502+ZebaSetas@users.noreply.github.com>
2021-06-01 19:14:01 -0300

    refactor env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,24 +1,6 @@
-// /**
-//  * @license
-//  * Copyright Akveo. All Rights Reserved.
-//  * Licensed under the MIT License. See License.txt in the project root for license information.
-//  */
-// export const environment = {
-//   production: true,
-//   apiEndpoint: 'http://centinela-env.eba-zhfdmg23.us-east-1.elasticbeanstalk.com/api/v1',
-// };
-
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
   production: false,
-  api: 'https://springboot-otravo-trips.herokuapp.com/otravo'
+  //api: 'http://localhost:8080/otravo'
+  api: 'https://springboot-otravo-trips.herokuapp.com/otravo/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,7 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 
 export const environment = {
   production: false,
-  //api: 'http://localhost:8080/otravo'
-  api: 'https://springboot-otravo-trips.herokuapp.com/otravo'
+ // api: 'http://localhost:8080/otravo/v1'
+  api: 'https://springboot-otravo-trips.herokuapp.com/otravo/v1'
 };
